24e21b8faf479ea22c64eade0376d889133eb5d1
Finikys <foroncefinikys@gmail.com>
2023-01-05 16:18:37 +0300

    Initial Commit

---
+++ src/main/java/org/example/AfishaManager.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class AfishaManager {
+    private String[] films = new String[0];
+    private int limit;
+    public  AfishaManager(){
+        this.limit = 10;
+    }
+    public AfishaManager(int limit){
+        this.limit = limit;
+    }
+
+    public void addFilm(String film){
+        String[] tmp = new String[films.length + 1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = film;
+        films = tmp;
+    }
+
+    public String[] findAll(){
+        return films;
+    }
+
+    public String[] findLast(){
+        int resultsLenght;
+        if (films.length < limit){
+            resultsLenght = films.length;
+        } else {
+            resultsLenght = limit;
+        }
+        String[] tmp = new String[resultsLenght];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = films[films.length - 1 - i];
+        }
+        return tmp;
+
+    }
+
+
+}
---
+++ src/test/java/FilmManagerTest.java
@@ -0,0 +1,83 @@
+import org.example.AfishaManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FilmManagerTest {
+
+    @Test
+    public void AllTest() {
+        String film1 =  "Film I";
+        String film2 =  "Film II";
+        String film3 =  "Film III";
+        String film4 =  "Film IV";
+        String film5 =  "Film V";
+        String film6 =  "Film VI";
+        String film7 =  "Film VII";
+        String film8 =  "Film VIII";
+        String film9 =  "Film IX";
+        String film10 =  "Film X";
+        String film11 =  "Film XI";
+        String film12 =  "Film XII";
+
+        AfishaManager manager = new AfishaManager();
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+        manager.addFilm(film4);
+        manager.addFilm(film5);
+        manager.addFilm(film6);
+        manager.addFilm(film7);
+        manager.addFilm(film8);
+        manager.addFilm(film9);
+        manager.addFilm(film10);
+        manager.addFilm(film11);
+        manager.addFilm(film12);
+
+        String[] expected = { "Film I","Film II","Film III","Film IV","Film V","Film VI","Film VII","Film VIII","Film IX","Film X","Film XI","Film XII"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void LastTest() {
+        String film1 = "Film I";
+        String film2 = "Film II";
+        String film3 = "Film III";
+        String film4 = "Film IV";
+        String film5 = "Film V";
+        String film6 = "Film VI";
+        String film7 = "Film VII";
+        String film8 = "Film VIII";
+        String film9 = "Film IX";
+        String film10 = "Film X";
+        String film11 = "Film XI";
+        String film12 = "Film XII";
+
+        AfishaManager manager = new AfishaManager();
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+        manager.addFilm(film4);
+        manager.addFilm(film5);
+        manager.addFilm(film6);
+        manager.addFilm(film7);
+        manager.addFilm(film8);
+        manager.addFilm(film9);
+        manager.addFilm(film10);
+        manager.addFilm(film11);
+        manager.addFilm(film12);
+
+        String[] expected = {"Film XII", "Film XI", "Film X", "Film IX", "Film VIII", "Film VII", "Film VI", "Film V", "Film IV", "Film III"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    
+
+
+
+
+
+}
